a7ff57f24aa28a5cc25f0200428a2ef6ec6e772f
Alexander Orvik <alexao1111@gmail.com>
2023-08-23 22:32:13 +0200

    nuke MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://orvik.dev',
-  integrations: [mdx(), sitemap()],
+  site: "https://orvik.dev",
+  integrations: [sitemap()],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
 });
