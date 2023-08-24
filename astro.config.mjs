31177c81c2a28e26727bbae4f48e66447be52c55
Alexander Orvik <alexao1111@gmail.com>
2023-08-24 10:51:37 +0200

    feat: add back mdx support
    I don't think this is the culrpit anymore. I blame yarn and cloudlare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import cloudflare from "@astrojs/cloudflare";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://orvik.dev",
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()],
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare()
 });
